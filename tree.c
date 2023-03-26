d3e7bc38eb4b6c629024974690d6a77f12028ea2
Sonia Bogdańska <soniabogdanska5@MacBook-Air-Sonia.local>
2023-03-26 22:21:10 +0200

    New function printing tree

--- tree.c
+++ tree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "tree.h"
 #include "heap.h"
 
@@ -25,3 +26,46 @@ node_t * build_huffman_tree( int freq[], int n_freq )
     return min_node_extract( heap );
 }
 
+void print_subtree( node_t * root, int indent, int is_right ) {
+    bool * right = malloc( 256 * sizeof( bool ) );
+    for( int i = 0; i < 256; i++ )
+        right[i] = 0;
+    
+    right[indent] = is_right;
+
+    if( root == NULL )
+        return;
+
+    int i;
+    //all levels until (without) level above current level
+    for ( i = 0; i < indent; i++ )
+    {
+        printf( "%s", right[i] ? "   " : "|  " );
+    }
+
+    //level directly above current level
+    if ( i <= indent )
+    {
+        printf( "%s", right[i] ? "└─" : "├─" ) ;
+    }
+    
+    //current level
+    if ( right[i] ){
+        printf( "(1)%d\n", root->data );
+    } else {
+        printf( "(0)%d\n", root->data );
+    }
+
+    //child levels
+    print_subtree( root->left, indent + 1, 0 );
+    print_subtree( root->right, indent + 1, 1 );
+    
+}
+
+void print_tree( node_t * root ) {
+    if( root != NULL ) {
+        printf( "%d\n", root->data );
+        print_subtree( root->left,  0, 0 );
+        print_subtree( root->right, 0, 1 );
+    }
+}
